bcf34b33fbb2e00b98773ce27e9ec425ce7647e9
saimaheshwaran <saimaheshwaran@outlook.com>
2024-05-12 08:46:45 -0400

    Removed Azure removed

--- src/main/java/com/tools/tms/restcontrollers/DefectRestController.java
+++ src/main/java/com/tools/tms/restcontrollers/DefectRestController.java
@@ -1,15 +1,14 @@
 package com.tools.tms.restcontrollers;
 
 import com.tools.tms.entities.Defect;
-import com.tools.tms.repositories.DefectRepository;
 import com.tools.tms.services.DefectService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -25,7 +24,7 @@ public class DefectRestController {
     }
 
     @PostMapping("/bg/update")
-    public ResponseEntity <Defect>updateDefects(@RequestBody Defect defect) {
+    public ResponseEntity<Defect> updateDefects(@RequestBody Defect defect) {
         return new ResponseEntity<Defect>(defectService.update(defect), HttpStatus.OK);
     }
 
